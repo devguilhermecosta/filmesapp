ee862564e91939f8e94257892eb257835925551a
guilherme costa <guilherme.partic@gmail.com>
2024-01-04 22:27:40 -0300

    Initial Commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
-module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+export const config = {
+  semi: true,
   trailingComma: 'all',
-};
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto',
+}
---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface FilmeProps {
+  id: string;
+  nome: string;
+  sinopse: string;
+  foto: string;
+}
