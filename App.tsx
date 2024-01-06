0e41095890405890c708a9e0a9ed4b69d5d3bd04
guilherme costa <guilherme.partic@gmail.com>
2024-01-06 11:54:22 -0300

    update: add the loading indicator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
 import { FilmProps } from './src/interfaces';
 import api from './src/services/api';
 import FilmItem from './src/components/Film';
 
 export default function App(): React.JSX.Element {
   const [films, setFilms] = useState<FilmProps[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getFilmes() {
       await api.get('r-api/?api=filmes')
-      .then((r) => setFilms(r.data));
+      .then((r) => {
+        {setFilms(r.data)};
+        setLoading(false);
+      });
     }
 
     getFilmes();
 
   }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large' color='#121212'/>
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
@@ -33,5 +45,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 10,
-  }
+  },
+  loadingContainer: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
 })
